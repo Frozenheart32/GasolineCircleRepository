4836f06dc648e0d66047512a0440472d05e58b7f
Frozenheart32 <alexroid32@gmail.com>
2020-11-17 20:26:59 +0300

    Create Template Main Menu, write basic logic MainMenu, create class Settings for save data about records of players and volume audio.

---
+++ Gasoline Circle/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/// <summary>
+/// Класс отвечающий за логику главного меню
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Запускает 1-ый уровень
+    /// </summary>
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Выход из игры
+    /// </summary>
+    public void ExitGame() 
+    {
+        Application.Quit();
+    }
+}
---
+++ Gasoline Circle/Assets/Scripts/Settings.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Класс отвечающий за хранение информации об игровом процессе между игровыми сценами
+/// </summary>
+[CreateAssetMenu(fileName = "NewSettings", menuName = "Create SettingsScrObj", order = 51)]
+public class Settings : ScriptableObject
+{
+    /// <summary>
+    /// Громкость аудио
+    /// </summary>
+    public float volume = 1f;
+
+    /// <summary>
+    /// Рекорд игрока №1
+    /// </summary>
+    public int PlayerOneHiScore = 0;
+    /// <summary>
+    /// Рекорд игрока №2
+    /// </summary>
+    public int PlayerTwoHiScore = 0;
+}
